6145342d0eb644ca48c278891375112f6dd7a8e1
Pierre Pellegrin <informatique@check-kit.com>
2018-04-29 13:43:17 +0200

    Implement search bar input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBONvANILeqJRE8SXVVbF_o5yFO4RcL5Co';
+
 const App = () => {
-    return <div>Hi !</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
